ff2682f0668d678ff81da09beab3ec8f22e5d2fc
Danelc <danelb28@gmail.com>
2017-10-14 18:36:44 +0300

    removed an unused old way of getting phrases

--- app/src/main/java/com/YouFool/danelc/youfool/MyBot.java
+++ app/src/main/java/com/YouFool/danelc/youfool/MyBot.java
@@ -194,58 +194,7 @@ public class MyBot extends PircBot  {
             player=1;
         }
         }
-
     }
-    //private String GetSentence()
-    //{
-    //    Random rnd = new Random();
-    //    switch (rnd.nextInt(45)){
-    //        case 0:return "1)A spectator in an Illinois courtroom was sentenced to six months in jail for *YAWNING* during a trial.";
-    //        case 1:return "2)In 2007, a woman with a rare disorder that causes her to be sexually attracted to inanimate objects married *THE EIFFEL TOWER*. ";
-    //        case 2:return "3)During the 1990s, teachers in North Korea were, oddly enough, required to know how to *PLAY THE ACCORDION*.";
-    //        case 3:return "4)A man in Milford, Iowa, got fired from his job after he used a *FORKLIFT* to dislodge his Twix bar from the office vending machine.";
-    //        case 4:return "5)Shiro Cosmetics sells an eye shadow called “*NIC CAGE* Raking Leaves on a Brisk October Afternoon.”";
-    //        case 5:return "6)In 2008, a couple was killed in South Africa while having sex on a *RAILROAD TRACK*.";
-    //        case 6:return "7)Walter Arnold received the world’s first speeding ticket in 1896 for going *8* miles per hour.";
-    //        case 7:return "8)Owning 55,000 of them, Ted Turner has the world’s largest private collection of *BISONS*.";
-    //        case 8:return "9)A 2013 survey conducted by Playtex revealed that 8% of Canadians have had sex in a *CANOE*.";
-    //        case 9:return "10)ROAD TRIP! When on the Hawaiian island of Molokai, be sure to visit the Hoolehua post office where you’re able to mail *COCONUTS* without using a package.";
-    //        case 10:return "11)A man from Enniskillen, Northern Ireland was sentenced to three months in prison for a fire he started while trying to turn *HIS POOP* into gold.";
-    //        case 11:return "12)In 2013 a zoo in China attempted to pass off a *LARGE DOG* as an African lion.";
-    //        case 12:return "13)In 1988, George H. W. Bush celebrated Halloween by dressing up as *HIMSELF*.";
-    //        case 13:return "14)Tashirojima is an island off of Japan that is completely overrun by *CATS*.";
-    //        case 14:return "15)In 1967, a small town in Ecuador elected an inanimate object mayor. The elected mayor was a *FOOT POWDER*.";
-    //        case 15:return "16)Although very unconventional, farmer William von Schneidau feeds his pigs *WEED*.";
-    //        case 16:return "17)In a 2013 poll conducted by Public Policy Polling, 27% of voters thought *HIPSTERS* should be subjected to a special tax for being so annoying.";
-    //        case 17:return "18)A study published in the journal Anthrozoo reported that cows produce 5% more milk when they are given *NAMES*.";
-    //        case 18:return "19)Metrophilia is sexual arousal caused by *POETRY*.";
-    //        case 19:return "20)The real name of the Internet’s famous Grumpy Cat is *TARTAR SAUCE*.";
-    //        case 20:return "21)In 2013, Dell issued a recall after customers complained their laptops smelled like *CAT URINE*.";
-    //        case 21:return "22)Tashirojima is an island off of Japan that is completely overrun by *CATS*.";
-    //        case 22:return "23)Anatidaephobia is the fear that somewhere in the world a *DUCK* is watching you.";
-    //        case 23:return "24)The Isle of Man flag depicts three *LEGS* interlinked together.";
-    //        case 24:return "25)A sequel to Beetlejuice titled Beetlejuice Goes *Hawaiian* was written but never made.";
-    //        case 25:return "26)Famed American poet Charles Bukowski’s tombstone is engraved with the words “Don’t *TRY*.”";
-    //        case 26:return "27)An eight-foot-tall *LEGO MAN* washed up ashore a Florida beach in 2011.";
-    //        case 27:return "28)Advanced Comfort Technology makes waterbeds for *COWS*.";
-    //        case 28:return "29)In 1976, boxing legend Muhammad Ali released an educational children’s album titled “Ali and His Gang Vs. Mr. *TOOTH DECAY*.";
-    //        case 29:return "30)Edward Smith from Yelm, Washington garnered media attention for his claim that he’s had sex with over 1,000 *CARS*.";
-    //        case 30:return "31)In October of 2013, eight sixth-graders from a New York college prep school were hospitalized after someone released *AXE BODY SPRAY* in a classroom.";
-    //        case 31:return "32)After an allergic reaction to steroids used to treat asthma, a 28-year-old woman started growing *FINGERNAILS* on her head instead of hair.";
-    //        case 32:return "33)In a 1999 interview with US Weekly, actress Lucy Liu revealed that she once had sex with a *GHOST*.";
-    //        case 33:return "34)The city of Olney, Illinois organizes an annual event in order to *COUNT* squirrels.";
-    //        case 34:return "35)ROAD TRIP! While in Florence, New Jersey be sure to check out the auto repair shop which, according to its owner, has an operational toilet once owned by *HITLER*.";
-    //        case 35:return "36)Amerigo Vespucci, the man for whom America was named, was a *PICKLE* dealer.";
-    //        case 36:return "37)Edgar Valdez Villarreal, a notorious Mexican drug cartel leader, had the not-so-scary nickname “La *BARBIE*.”";
-    //        case 37:return "38)A group known as the “Robin Hooders” in Keene, New Hampshire pay for other people’s *PARKING METERS*.";
-    //        case 38:return "39)A man in western Pennsylvania got a “Driving Under Influence” ticket for having an open can of beer while riding a *LAWN MOWER*.";
-    //        case 39:return "40)A beaver’s anal secretions can be used as a *VANILLA* substitute in some foods.";
-    //        case 40:return "41)According to the International Code of Disease, the code Y92250 is the code for “when a patient is injured in an *ART GALLERY*.”";
-    //        case 41:return "42)In 2013, a dad in China hired pro *GAMERS* to kill his son, so the son would start looking for a job and get a life.";
-    //        case 42:return "43)In India you can buy a cola named Gau Jal that includes the bizarre ingredient *COW URINE*.";
-    //        case 43:return "44)On January 13, 2014, U.S. Secretary of State John Kerry presented to Russian Foreign Minister Sergei Lavrov the odd gift of two very large *POTATOES*.";
-    //        case 44:return "45)The inventor of the chocolate chip cookie traded her recipe to Nestlé in exchange for a *LIFE TIME SUPPLY OF CHOCOLATE*.";
-    //    }
 
     public void SendLie(String s)
     {
